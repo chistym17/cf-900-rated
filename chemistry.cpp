4e46901fdee9726bc0bc352920c99a98fffc667b
chistym17 <mchisty31@gmail.com>
2024-03-10 21:03:47 +0600

    first commit

---
+++ chemistry.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+int t;
+cin>>t;
+
+while(t--)
+{
+int n;
+cin>>n;
+vector<int>arr;
+
+for(int i=0;i<n;i++ )
+{int num;
+cin>>num;
+arr.push_back(num);
+}
+
+
+
+}
+
+
+
+
+return 0;
+}
